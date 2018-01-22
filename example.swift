1b8598978bbab331efca52647c125f0685e75b49
iyeongjun <they6687@naver.com>
2018-01-22 20:05:37 +0900

    add Protocol

---
+++ example.swift
@@ -0,0 +1,14 @@
+//
+// Created by leeyoungjoon on 2018. 1. 20..
+//
+
+import Foundation
+
+class example {
+    let myLet : String = ""
+
+    func myFun(callback myLet : String) -> String{
+        return myLet
+    }
+    
+}
