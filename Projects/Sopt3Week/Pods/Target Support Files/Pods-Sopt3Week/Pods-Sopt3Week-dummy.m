50fe26b7a7904dd3536cc918dc5d988c3a798609
iyeongjun <they6687@naver.com>
2018-04-14 15:17:08 +0900

    add Sopt

---
+++ Projects/Sopt3Week/Pods/Target Support Files/Pods-Sopt3Week/Pods-Sopt3Week-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sopt3Week : NSObject
+@end
+@implementation PodsDummy_Pods_Sopt3Week
+@end
---
+++ Projects/Sopt3Week/Sopt3Week/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sopt3Week
+//
+//  Created by 이영준 on 2018. 4. 14..
+//  Copyright © 2018년 이영준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
